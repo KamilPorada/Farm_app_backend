e438062167736be6faa420c05402c14ca37eb364
Kamil0k <kamil.porada@onet.pl>
2026-01-12 15:59:18 +0100

    Initial commit

---
+++ src/main/java/pl/farmapp/backend/FarmAppBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.farmapp.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/farmapp/backend/FarmAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.farmapp.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
